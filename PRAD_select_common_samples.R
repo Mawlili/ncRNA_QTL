7b419cb7365083f53a7ebb08fecfc290f30ad16e
William Wu <149866883+Mawlili@users.noreply.github.com>
2025-03-29 21:58:20 -0500

    Create PRAD_select_common_samples.R

---
+++ PRAD_select_common_samples.R
@@ -0,0 +1,40 @@
+library(data.table)
+for file in *.vcf.vcf; do
+  bgzip -f "$file"         # creates file.vcf.vcf.gz
+  tabix -p vcf "${file}.gz"  # creates file.vcf.vcf.gz.tbi
+done
+vcf-merge *.vcf.vcf.gz > PRAD_merged.vcf
+bcftools query -l PRAD_merged.vcf > sample_list.txt
+sed 's/.*TCGA-PRAD_//' sample_list.txt > cleaned_sample_ids.txt 
+awk -F'-' '{print $1"-"$2"-"$3}' cleaned_sample_ids.txt  > short_TCGA_vcf_list.txt
+
+
+samples <- scan("/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/vcf/PRAD/short_TCGA_vcf_list.txt", what = "", sep = "\t", quiet = TRUE)
+short_non_coding <- fread("/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/PRAD_qtl/input/short_non_coding.bed")
+short_non_coding <- as.data.frame(short_non_coding)
+short_coding <- fread("/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/PRAD_qtl/input/short_coding.bed")
+short_coding <- as.data.frame(short_coding)
+cov <- fread("/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/PRAD_qtl/input/short_cov_V250.txt")
+cov <- as.data.frame(cov)
+
+bed_cols <- c("Chr", "start", "end", "pid", "gid", "strand")
+cov_col <- c("TCGA_ID")
+
+overlap_cols_non_coding <- intersect(colnames(short_non_coding), samples)
+overlap_cols_coding     <- intersect(colnames(short_coding), samples)
+subset_non_coding <- short_non_coding[ , c(bed_cols, overlap_cols_non_coding)]
+subset_coding     <- short_coding[, c(bed_cols, overlap_cols_coding)]
+overlap_cov <- intersect(colnames(cov), samples)
+subset_cov <- cov[,c(cov_col, overlap_cov)]
+
+colnames(subset_non_coding)[1] <- "#Chr"
+colnames(subset_coding)[1] <- "#Chr"
+
+write.table(subset_non_coding, "/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/PRAD_qtl/input/short_non_coding_overlap.bed",
+            sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+write.table(subset_coding, "/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/PRAD_qtl/input/short_coding_overlap.bed",
+            sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+write.table(subset_cov, "/rsrch5/home/epi/bhattacharya_lab/projects/ncRNA_QTL/PRAD_qtl/input/short_cov_overlap_V250.txt",
+            sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
